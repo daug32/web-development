21071ec9da0361a41c253e953c3f1f2cd7f92a93
Dmitry_Ivanov <daug32@mail.ru>
2022-02-24 08:22:31 +0300

    main
    Edited second lab in according to task

--- lw3/task2.php
+++ lw3/task2.php
@@ -13,22 +13,22 @@ function CheckIdentifier($id)
 {
     $length = strlen($id);
     if($length < 1 || !ctype_alpha($id[0])) 
-        return 'no';
+        return "No. Identifier can start only from a letter.";
+        
     for($i = 1; $i < $length; $i++)
     {
         $cond = is_numeric($id[$i]) || ctype_alpha($id[$i]);
-        if(!$cond) return 'no';
+        if(!$cond) return "No. Identifier have to contain only numbers and letters - no other symbols";
     }
-    return 'yes';
+    return "Yes.";
 }
 
-$id = $_GET["identifier"];
-echo CheckIdentifier($id);
+// $id = $_GET["identifier"];
+// echo CheckIdentifier($id);
 
-// require '../tester.php';
-// TestFunction('a', 'CheckIdentifier');
-// TestFunction('abc', 'CheckIdentifier');
-// TestFunction('a44', 'CheckIdentifier');
-// TestFunction('a44b', 'CheckIdentifier');
-// TestFunction('44a', 'CheckIdentifier');
-// TestFunction('44', 'CheckIdentifier');
+require '../tester.php';
+TestFunction('', 'CheckIdentifier');
+TestFunction('abc', 'CheckIdentifier');
+TestFunction('a44b', 'CheckIdentifier');
+TestFunction('44a', 'CheckIdentifier');
+TestFunction('a#s', 'CheckIdentifier');
